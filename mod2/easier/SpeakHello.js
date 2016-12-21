d1e99808b8879754a3d2313fed560b9b0541b1eb
mayowa salami <mayowa salami>
2016-12-21 15:11:30 +0100

    new

--- mod2/easier/SpeakHello.js
+++ mod2/easier/SpeakHello.js
@@ -1,4 +1,4 @@
-(function(window){
+(function(window) {
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
@@ -8,6 +8,8 @@
 // See Lecture 52, part 1
 // var helloSpeaker =
  var helloSpeaker = {};
+
+
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
 
@@ -18,10 +20,12 @@ helloSpeaker.speak = function(name) {
   console.log(speakWord + " " + name);
 }
 
+
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 
-window.helloSpeaker = helloSpeaker;
+window.helloSpeaker=helloSpeaker;
+
 })(window);
